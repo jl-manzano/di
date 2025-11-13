50c9b140dc7ce4121e6d8e3b98c10b1243239cb5
jl-manzano <jl.manzano@iesnervion.es>
2025-11-13 01:57:06 +0100

    REPASO

--- Unidad5/Ejercicio2/app/src/usecases/getUsersUseCase.ts
+++ Unidad5/Ejercicio2/app/src/usecases/getUsersUseCase.ts
@@ -1,13 +1,6 @@
 // src/usecases/getUsersUseCase.ts
 import { getUsers } from '../repositories/userRepository';
 
-export const fetchUsersUseCase = async (userAge: number) => {
-  try {
-    const users = await getUsers();
-    return users.filter(
-      (u: { age: number }) => Math.abs(u.age - userAge) <= 5
-    );
-  } catch {
-    throw new Error('Failed to fetch users');
-  }
+export const getUsersUseCase = (userAge: number) => {
+  return getUsers(userAge);
 };
